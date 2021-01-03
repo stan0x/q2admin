247252420b04d55045ca8ac22584c2c627a5f4b2
stan0x <stan0x@gmail.com>
2021-01-03 03:35:10 +0100

    oops forgot something :(

--- src/q2a_main.c
+++ src/q2a_main.c
@@ -27,7 +27,7 @@ void *hdll = NULL;
 #elif defined (__arm__)
 #define DLLNAME         "gamearm.real.so"
 #elif defined (__aarch64__)
-#define DLLNAME         "gameaarch.real.so"
+#define DLLNAME         "gameaarch64.real.so"
 #endif
 
 #endif
